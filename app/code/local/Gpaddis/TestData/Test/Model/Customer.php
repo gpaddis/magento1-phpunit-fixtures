58068be22d78bb162bf6dd26b57adf187fca4748
Gianpiero Addis <g.addis@lemundo.de>
2018-11-02 17:56:23 +0100

    Load customer email address

--- app/code/local/Gpaddis/TestData/Test/Model/Customer.php
+++ app/code/local/Gpaddis/TestData/Test/Model/Customer.php
@@ -12,7 +12,7 @@ class Gpaddis_TestData_Test_Model_Customer extends EcomDev_PHPUnit_Test_Case
     /**
      * @test
      * @loadFixture ~Gpaddis_TestData/shop
-     * @loadFixture ~Gpaddis_TestData/customers
+     * @loadFixture ~Gpaddis_TestData/customer
      */
     public function it_loads_the_customers()
     {
@@ -20,8 +20,10 @@ class Gpaddis_TestData_Test_Model_Customer extends EcomDev_PHPUnit_Test_Case
 
         $customer->load(1);
         $this->assertEquals('John Doe', $customer->getName());
+        $this->assertEquals('john@doe.com', $customer->getEmail());
 
         $customer->load(2);
         $this->assertEquals('Jane Doe', $customer->getName());
+        $this->assertEquals('jane@doe.com', $customer->getEmail());
     }
 }
