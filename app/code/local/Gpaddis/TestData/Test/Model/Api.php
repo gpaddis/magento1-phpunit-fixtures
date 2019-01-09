3d22167831e672e541d76f7554a798d960e8f534
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-01-09 10:36:51 +0100

    Add example tests for the standard API (#2)
    * Edit README
    
    * Setup methods to create Api role and user
    
    * Add unit tests for the Magento API
    
    * Fix issue with indexer on unit test

---
+++ app/code/local/Gpaddis/TestData/Test/Model/Api.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @package Gpaddis_TestData
+ * @author  Gianpiero Addis <g.addis@lemundo.de>
+ *
+ * @group Gpaddis_TestData
+ */
+class Gpaddis_TestData_Test_Model_Api extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * @test
+     * @loadFixture registry
+     * @loadFixture shop
+     * @loadFixture category
+     */
+    public function test_catalog_category_info()
+    {
+        $apiModel = Mage::getModel('catalog/category_api');
+        $categoryInfo = $apiModel->info(2);
+
+        $this->assertEquals('Default Category', $categoryInfo['name']);
+    }
+
+    /**
+     * @test
+     * @loadFixture registry
+     * @loadFixture shop
+     * @loadFixture product
+     */
+    public function test_catalog_product_info()
+    {
+        $apiModel = Mage::getModel('catalog/product_api');
+        $productInfo = $apiModel->info(3);
+
+        $this->assertEquals('configurable-product', $productInfo['name']);
+    }
+
+    /**
+     * @test
+     * @loadFixture registry
+     * @loadFixture shop
+     * @loadFixture product
+     */
+    public function test_catalog_product_update()
+    {
+        $apiModel = Mage::getModel('catalog/product_api');
+
+        $apiModel->update(2, ['sku' => 'new-sku']);
+        $productInfo = $apiModel->info(2);
+
+        $this->assertEquals('new-sku', $productInfo['sku']);
+    }
+}
